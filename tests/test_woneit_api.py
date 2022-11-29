a9a5c1ad942b0c08d80158e3ea1fc3b43a41db1c
Olga <ok2304@yandex.ru>
2022-11-29 17:39:23 +0300

    fix

--- tests/test_woneit_api.py
+++ tests/test_woneit_api.py
@@ -10,3 +10,39 @@ def test_get_good_requiest():
 def test_get_bad_request():
     response = reqres_session().get(url='/1217/')
     assert response.status_code == 404
+
+
+'''
+import allure
+from allure_commons.types import Severity
+
+from utils.base_session import reqres_session
+
+
+@allure.tag("api")
+@allure.severity(Severity.NORMAL)
+@allure.label("olgakos")
+@allure.description("Description: Status code is 200")
+@allure.feature("Feature: Allure with decorating")
+@allure.story("Decorator")
+@allure.link("https://wone-it.ru")
+def test_get_good_requiest():
+    with allure.step("Open page"):
+        response = reqres_session().get(url='/company/')
+    with allure.step("Verify the expected response"):
+        assert response.status_code == 200
+
+
+@allure.tag("api")
+@allure.severity(Severity.NORMAL)
+@allure.label("olgakos")
+@allure.description("Description: Status code is 404")
+@allure.feature("Feature: Allure with decorating")
+@allure.story("Decorator")
+@allure.link("https://wone-it.ru")
+def test_get_bad_request():
+    with allure.step("Open page"):
+        response = reqres_session().get(url='/1217/')
+    with allure.step("Verify the expected response"):
+        assert response.status_code == 404
+'''
