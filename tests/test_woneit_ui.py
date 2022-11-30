56cfa81e79c1c9430cf6f1f465619c235741cd6b
Olga <49266934+olgakos@users.noreply.github.com>
2022-11-30 11:47:02 +0300

    Update test_woneit_ui.py

--- tests/test_woneit_ui.py
+++ tests/test_woneit_ui.py
@@ -16,13 +16,13 @@ from models.page_projects import *
 def setup_browser():
     browser.config.timeout = 3
     browser.config.browser_name = 'chrome'
-    browser.config.base_url = 'https://wone-it.ru' #?
+    browser.config.base_url = 'https://wone-it.ru' #!!
     browser.config.window_width = 1280
     browser.config.window_height = 1024
     yield
-#запускается в Дж., но
+#Важно! Если не убрать это из тестов в конфтест, то: стартанет в Дж., Аллюре не соберет UI тесты,
+ошибка: 
 #selenium.common.exceptions.WebDriverException: Message: 'chromedriver' executable needs to be in PATH. Please see https://chromedriver.chromium.org/home
-сообщение в Аллюре при старте из Дж. UI тесты не пройдут
 '''
 
 @allure.tag("web")
